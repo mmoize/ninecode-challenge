8933e32c3acbf39939f0d8c002bcd60f654397cc
Moise Murhi <murhi46@gmail.com>
2021-04-16 18:05:09 +1000

    adding headers

--- app.js
+++ app.js
@@ -9,6 +9,21 @@ app.use(express.json());
 
 
 
+app.use((req, res, next) => {
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader(
+      "Access-Control-Allow-Headers",
+      "Origin, X-Requested-With, Content-Type, Accept"
+    );
+    res.setHeader(
+      "Access-Control-Allow-Methods",
+      "GET, POST, PATCH, DELETE, OPTIONS"
+    );
+    next();
+  });
+
+
+
 //Error Handler
 app.use((err, req, res, callback) => {
     const status = 400
