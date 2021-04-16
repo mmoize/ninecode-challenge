7537c66892a92b8bc7b2dd099e2ba0c6dbac1f7d
Moise Murhi <murhi46@gmail.com>
2021-04-16 19:12:22 +1000

    comments

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const app = express()
 app.use(express.json());
 
 
-
+//  adding headers
 app.use((req, res, next) => {
     res.setHeader("Access-Control-Allow-Origin", "*");
     res.setHeader(
@@ -35,7 +35,7 @@ app.use((err, req, res, callback) => {
     }
 
     const body = errors
-    res.setHeader('content-type', 'application/json');
+
     res.status(status).send(body)
     callback()
   })
@@ -65,7 +65,7 @@ app.post('/api/data', (req, res) => {
            error: "please check your payload  "+ error.details[0].message
        }
         const body = errors
-        res.setHeader('content-type', 'application/json');
+   
         res.status(status).send(body)
         return;
     };
@@ -86,11 +86,11 @@ app.post('/api/data', (req, res) => {
                 const status = 400
 
                 const errors = {
-                error: "please check your payload  "+ error.details[0].message
+                error: "please check your payload  "+ payLoadResults.error.details[0].message
                      }
 
                 const body = errors
-                res.setHeader('content-type', 'application/json');
+
                 res.status(status).send(body)
                 return;
             };
@@ -100,15 +100,16 @@ app.post('/api/data', (req, res) => {
 
    
 
-   //packing the response data into object.
+   //packing the response data into  a json response.
     const responseData = []
 
     for(let i of tvShows) {
-
+        //unpacking the required fields from the tvshow's data
         const image = i['image'].showImage
         const slug = i['slug']
         const title = i['title']
 
+        //retrieving shows with the field drm enabled with an episode count of > 0 
         if (i["drm"] === true && i['episodeCount'] > 0) {
             const data = {
                 image : image,
@@ -116,18 +117,16 @@ app.post('/api/data', (req, res) => {
                 title: title
             }
     
-            responseData.unshift(data)
+            responseData.unshift(data)  //adding the show in the responseData arraylist.
         }
-
-        
     }
 
-
+    // response json
     const response = {
         response: responseData
     }
 
-    res.setHeader('content-type', 'application/json');
+    
 
     res.send(response);
 })
@@ -141,7 +140,7 @@ app.listen(process.env.PORT || 3000, () => console.log('listing on port 3000'));
 
 
 
-// validator
+// validator for request body data
 function validateResultsResponse(data) {
     const schema = Joi.object({
         payLoad: Joi.array().required(),
@@ -154,7 +153,7 @@ function validateResultsResponse(data) {
 }
 
 
-//validator 
+//validator for the tvshow's data
 function validateResultsPayload(data) {
     const schema = Joi.object({
         country: Joi.string().required(),
